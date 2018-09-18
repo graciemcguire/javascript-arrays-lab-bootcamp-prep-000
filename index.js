a1fb8d02ce59df1bd060462a83a77303af98bdac
Gracie McGuire <m.graciemcguire@gmail.com>
2018-09-18 18:20:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your functions and code here
+function destructivelyAppendKitten(name){
+  //var kittens = ["Milo", "Otis", "Garfield"];
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  return kittens.pop(name);
+}
+
+function
